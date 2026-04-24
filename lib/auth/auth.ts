9bd37dcaaca690d025461a6468e5079d9e9cb0b1
ayush-trivedi-925 <asxu525@gmail.com>
2026-04-24 13:59:49 +0530

    database and better auth setup

---
+++ lib/auth/auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+import { prisma } from "@/lib/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+    minPasswordLength: 8,
+  },
+  // Add social providers here if you want them
+  // socialProviders: {
+  //   google: {
+  //     clientId: process.env.GOOGLE_CLIENT_ID!,
+  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+  //   },
+  // },
+  // plugins: [nextCookies()], // must be last — handles cookies in server actions
+});
